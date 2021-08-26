e105daa97addca4109d8db7a0448dc51a08d9731
Elena Vlasenko <olefelena@gmail.com>
2021-08-26 16:02:53 +0300

    added string for connecting to mongodb

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,31 @@
 const app = require('../app')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
-const { PORT = 3000 } = process.env
+// const express = require('express')
+// const cors = require('cors')
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
+dotenv.config()
+
+const { PORT = 3000, DB_HOST } = process.env
+
+// const app = express()
+// app.use(cors())
+// app.get('/', (req, res) => {
+//   res.send('<h2>Home page</h2>')
+// })
+
+mongoose.connect(DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  }).catch((err) => {
+    console.log(err)
+  })
+// app.listen(PORT, () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`)
+// })
