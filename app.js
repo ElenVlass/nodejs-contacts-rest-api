ae79057aea026ff905de83e89f16885ff0f90019
Elena Vlasenko <olefelena@gmail.com>
2021-08-26 16:07:19 +0300

    bugfix/change route path

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/contacts', contactsRouter)
+app.use('/', contactsRouter)
 
 app.use((_, res) => {
   res.status(404).json({
