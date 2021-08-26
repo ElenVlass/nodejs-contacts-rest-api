4b6ccb13ff86f15b2bfa77582ce25a738c05b307
Elena Vlasenko <olefelena@gmail.com>
2021-08-26 16:49:49 +0300

    change route backward

--- bin/server.js
+++ bin/server.js
@@ -2,18 +2,9 @@ const app = require('../app')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
-// const express = require('express')
-// const cors = require('cors')
-
 dotenv.config()
 
 const { PORT = 3000, DB_HOST } = process.env
-
-// const app = express()
-// app.use(cors())
-// app.get('/', (req, res) => {
-//   res.send('<h2>Home page</h2>')
-// })
 
 mongoose.connect(DB_HOST, {
   useNewUrlParser: true,
@@ -26,6 +17,3 @@ mongoose.connect(DB_HOST, {
   }).catch((err) => {
     console.log(err)
   })
-// app.listen(PORT, () => {
-//   console.log(`Server running. Use our API on port: ${PORT}`)
-// })
