3b9f2e5b03da6e39ad446658cb10e00c0829a1e6
Elena Vlasenko <olefelena@gmail.com>
2021-09-02 15:55:36 +0300

    + validationMiddleWare

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
 
 const { users: controller } = require('../../controllers')
+const { joiValidation } = require('../../validation')
+const { userJoiSchema } = require('../../models/user')
 
-router.post('/users/signup', controller.signup)
+const validationMiddleWare = joiValidation(userJoiSchema)
+
+router.post('/users/signup', validationMiddleWare, controller.signup)
 // router.post('/users/login', controller.login)
 // router.post('/users/logout', controller.logout)
 // router.post('/users/current', controller.current)
